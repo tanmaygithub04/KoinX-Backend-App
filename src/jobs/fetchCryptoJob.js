ad99d03877ad2c40a2e82e45933403d893f62f5b
Tanmay <tanmay@Tanmays-MacBook-Air.local>
2025-01-11 12:31:48 +0530

    background job fixed

--- src/jobs/fetchCryptoJob.js
+++ src/jobs/fetchCryptoJob.js
@@ -2,18 +2,25 @@ const cron = require('node-cron');
 const Crypto = require('../models/crypto');
 const fetchCryptoData = require('../services/fetchCryptoData');
 
+const fetchAndSaveData = async () => {
+  console.log('Fetching crypto data...');
+  try {
+    const cryptoData = await fetchCryptoData();
+    await Crypto.insertMany(cryptoData);
+    console.log('Crypto data updated successfully.');
+  } catch (error) {
+    console.error('Error updating crypto data:', error.message);
+  }
+};
+
 const startFetchCryptoJob = () => {
   console.log('Starting fetch crypto job...');
-  cron.schedule('0 */2 * * *', async () => {
-    console.log('Fetching crypto data...');
-    try {
-      const cryptoData = await fetchCryptoData();
-      await Crypto.insertMany(cryptoData);
-      console.log('Crypto data updated successfully.');
-    } catch (error) {
-      console.error('Error updating crypto data:', error.message);
-    }
-  });
+  
+  // Run immediately
+  fetchAndSaveData();
+  
+  // Then schedule for every 2 hours
+  cron.schedule('0 */2 * * *', fetchAndSaveData);
 };
 
 module.exports = startFetchCryptoJob;
